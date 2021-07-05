18b1a75d1de5bf9ef2dc2921b2998017e71399f9
luizmn <luizmn@outlook.com>
2021-07-05 11:54:22 +0100

    updated styles, created home page

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import styles from "./styles.module.scss";
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="App Logo" />
+        <nav>
+          <a className={styles.active}>Home</a>
+          <a>Posts</a>
+        </nav>
+      </div>
+    </header>
+  )
+}
