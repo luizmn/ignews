e0a622a9756c7d7b0da83d7c49ffb1958fbdef96
luizmn <luizmn@outlook.com>
2021-08-04 18:25:09 +0100

    added subscription status update

--- src/pages/api/_lib/manageSubscription.ts
+++ src/pages/api/_lib/manageSubscription.ts
@@ -5,15 +5,16 @@ import { stripe } from "../../../services/stripe";
 export async function saveSubscription(
   subscriptionId: string,
   customerId: string,
+  createAction = false,
 ) {
-  console.log("MANAGE SUBSCRIPTIONS")
+
 console.log(subscriptionId,customerId)
   const userRef = await fauna.query(
     q.Select(
-      'ref',
+      "ref",
       q.Get(
         q.Match(
-          q.Index('user_by_stripe_customer_id'),
+          q.Index("user_by_stripe_customer_id"),
           customerId
         )
       )
@@ -29,11 +30,29 @@ console.log(subscriptionId,customerId)
     price_id: subscription.items.data[0].price.id,
   }
 
-  await fauna.query(
-    q.Create(
-      q.Collection("subscriptions"),
-      { data: { subscriptionData } }
+  if (createAction) {
+    await fauna.query(
+      q.Create(
+        q.Collection("subscriptions"),
+        { data: subscriptionData }
+      )
     )
-  )
+  } else {
+    await fauna.query(
+      q.Replace(
+        q.Select(
+          "ref",
+          q.Get(
+            q.Match(
+              q.Index("subscription_by_id"),
+              subscription.id,
+            )
+          )
+        ),
+        { data: subscriptionData }
+      )
+    )
+  }
 }
+
 
